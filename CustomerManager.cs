65fd6ac487a0247da0d8ed092cc4b3a04c6dc9b7
Harrison Pusey <harrisonsp@users.noreply.github.com>
2017-03-08 08:56:47 -0700

    Adding WWW retrieval of JSON object from S3

---
+++ CustomerManager.cs
@@ -0,0 +1,216 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+//using LitJson;
+using UnityEngine.UI;
+
+public class CustomerManager : MonoBehaviour {
+
+
+	public CustomerRegistry myRegistry;
+	public bool saveOnStart;
+    private WWW CustomerJson; 
+
+	FurnitureManager FurnManager;
+	MaterialManager matManager;
+
+	// Use this for initialization
+	void Start() {
+		FurnManager = GameObject.FindObjectOfType<FurnitureManager> ();
+		matManager = GameObject.FindObjectOfType<MaterialManager> ();
+
+        Debug.Log("Initialize customers");
+        string url = "https://s3-us-west-2.amazonaws.com/holoture/JSONfiles/customerList.json";
+        CustomerJson = new WWW(url);
+
+        /*if (saveOnStart) {
+			//SerializeCustomers ();
+		}*/
+
+        StartCoroutine(getCustRegistry());
+		//InitializeCustomers();
+
+	}
+
+    public IEnumerator getCustRegistry()
+    {
+        Debug.Log("get Cust Registry");
+        yield return CustomerJson;
+
+       
+
+        if (CustomerJson.error == null)
+        {
+            //Debug.Log(CustomerJson.text);
+            myRegistry = JsonUtility.FromJson<CustomerRegistry>(CustomerJson.text);
+            //myRegistry = ProcessRegistryJson(CustomerJson.text);
+            InitializeCustomers();
+
+        }
+        else
+        {
+            Debug.Log("ERROR: " + CustomerJson.error);
+        }
+    }
+
+
+	void InitializeCustomers(){
+        /*
+		string registryName = "CustomerRegistry.txt";
+		string RegistryString = File.ReadAllText (registryName);
+        */
+
+             
+
+        foreach (CustomerInfo pair in myRegistry.customerList) {
+
+			foreach (FurniturePreference pref in pair.furnPrefs) {
+				pref.FurniturePick = FurnManager.GetFurniture (pref.FurnitureID);
+
+				foreach (int id in pref.MaterialIDs) {
+					pref.materialPicks.Add (matManager.GetMaterial (id));
+				}
+			}
+		}
+
+		//REMOVE THIS LATER
+		CustomerFurnitureMenu.instance.loadPreferences(myRegistry.customerList [0]);
+
+		//GameObject.FindObjectOfType<CustomerFurnitureMenu> ().CreateFurntiture (0);
+	}
+	
+/*	IEnumerator SerializeCustomers(){
+        /*
+        string registryName = "CustomerRegistry.txt";
+        string json = JsonUtility.ToJson(myRegistry);
+        File.Create(registryName);
+        File.SetAttributes(registryName, FileAttributes.Normal);
+        File.WriteAllText(registryName, json);
+       
+
+
+        Debug.Log("INitialize customers");
+        string url = "https://s3-us-west-2.amazonaws.com/holoture/JSONfiles/customerList.json";
+        WWW www = new WWW(url);
+        yield return www;
+        Debug.Log(www.text);
+
+        if (www.error == null)
+        {
+
+            //myRegistry = ProcessRegistryJson(www.text);
+            // string registryName = File.ReadAllText(www.text);
+            string registryName = "CustomerRegistry.txt";
+            string json = JsonUtility.ToJson(myRegistry);
+            File.Create(registryName);
+            File.SetAttributes(registryName, FileAttributes.Normal);
+            File.WriteAllText(registryName, json);
+            //myRegistry = JsonUtility.FromJson<CustomerRegistry>(RegistryString);
+        }
+        else
+        {
+            Debug.Log("ERROR: " + www.error);
+        }
+
+      
+    }*/
+
+	public CustomerInfo GetMaterial(int num){
+
+		foreach (CustomerInfo mat in myRegistry.customerList){
+			if (mat.CustomerID == num) {
+				return mat;
+			}}
+		throw new System.Exception("Customer Not Found!");
+
+	}
+
+
+
+	[Serializable]
+	public class CustomerRegistry
+	{
+		public List<CustomerInfo> customerList;
+	}
+
+	[Serializable]
+	public class CustomerInfo
+	{
+		public string CustomerName;
+		public int CustomerID;
+		public List<FurniturePreference> furnPrefs;
+	}
+
+
+	[Serializable]
+	public class FurniturePreference
+	{
+
+		public int FurnitureID;
+		public List<int> MaterialIDs;
+		public FurniturePreference(FurnitureManager.FurniturePairing pair)
+		{
+			FurnitureID = pair.id;
+			MaterialIDs = new List<int>();
+			MaterialIDs.Add(pair.DefaultMaterial);
+			FurniturePick = FurnitureManager.instance.GetFurniture (pair.id);
+
+			foreach (int id in MaterialIDs) {
+				materialPicks.Add (MaterialManager.instance.GetMaterial (id));
+			}
+		}	public FurniturePreference()
+	{
+	}
+
+		[NonSerialized]
+		public FurnitureManager.FurniturePairing FurniturePick;
+		[NonSerialized]
+		public List<MaterialManager.MaterialPairing> materialPicks = new List<MaterialManager.MaterialPairing>();
+
+	}
+
+/*
+   private CustomerRegistry ProcessRegistryJson(string jsonString)
+    {
+        // convert to json-like object with LitJSON library
+        JsonData jsonvalue = JsonMapper.ToObject(jsonString);
+        //Debug.Log(jsonvalue);
+        CustomerRegistry newRegistry = new CustomerRegistry();
+
+        // get each customer
+        for (int i = 0; i < jsonvalue["customerList"].Count; i++)
+        {
+            CustomerInfo newPref = new CustomerInfo();
+            newPref.CustomerName = jsonvalue["customerList"][i]["CustomerName"].ToString();
+            Debug.Log(newPref.CustomerName);
+            newPref.CustomerID = (int)jsonvalue["customerList"][i]["CustomerID"];
+            Debug.Log(newPref.CustomerID);
+
+            //get each Furniture Preferences for the customer
+            for ( int j = 0; j < jsonvalue["customerList"][i]["furnPrefs"].Count; j++){
+                FurniturePreference myPref = new FurniturePreference();
+                myPref.FurnitureID = (int)jsonvalue["customerList"][i]["furnPrefs"]["FurnitureID"];
+                Debug.Log(myPref.FurnitureID);
+
+                // get each material for each preference of the customer
+                // This doesn't work because the data member being accessed is
+                for(int k = 0; k < jsonvalue["customerList"][i]["furnPrefs"]["MaterialIDs"].Count; k++)
+                {
+                    Debug.Log((string)jsonvalue["customerList"][i]["furnPrefs"]["MaterialIDs"][k]);
+                    myPref.MaterialIDs.Add((int)jsonvalue["customerList"][i]["furnPrefs"]["MaterialIDs"][k]);
+                }
+
+            }
+           newRegistry.customerList.Add(newPref);
+        }
+
+        //return instatiated and fairly fleshed out CustomerRegistry object.
+        return newRegistry;
+    }
+
+    */
+
+
+}
---
+++ FurnitureManager.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+//using LitJson;
+using UnityEngine.UI;
+
+public class FurnitureManager : MonoBehaviour {
+
+
+	public enum FurnitureType
+	{
+		Chair, Table, Couch, Bed, Any
+	}
+	public FurnitureRegistry myRegistry;
+	public bool saveOnStart; 
+	public static FurnitureManager instance;
+    private WWW registryJson;
+
+	// Use this for initialization
+    // make an OnStart Function
+	void Awake() {
+        Debug.Log("Awake");
+		instance = this;
+		if (saveOnStart) {
+		   // SerializeFurniture ();
+		}
+        InitializeFurniture();
+        /*
+         * Add this back in later if we want to allow different furniture registries.
+         * No current need.
+        string url = "https://s3-us-west-2.amazonaws.com/holoture/JSONfiles/registry.json";
+        registryJson = new WWW(url);
+        StartCoroutine(getRegistry());
+        */
+
+
+    }
+
+
+    public IEnumerator getRegistry()
+    {
+       
+        Debug.Log("get Furn registry");
+        yield return registryJson;
+       
+
+        if (registryJson.error == null)
+        {
+            Debug.Log(registryJson.text);
+            myRegistry = JsonUtility.FromJson<FurnitureRegistry>(registryJson.text);
+            InitializeFurniture();
+
+        }
+        else
+        {
+            Debug.Log("ERROR: " + registryJson.error);
+        }
+    }
+
+	void InitializeFurniture(){
+        /*
+		string registryName = "FurnitureRegistry.txt";
+		string RegistryString = File.ReadAllText (registryName);
+		myRegistry = JsonUtility.FromJson<FurnitureRegistry> (RegistryString);
+
+        */
+
+        /*       Links that were helpful to Harrison
+        
+        http://answers.unity3d.com/questions/935800/read-json-file-data-which-saved-in-server.html
+        https://forum.unity3d.com/threads/www-is-not-ready-downloading-yet.131989/
+        http://lbv.github.io/litjson/docs/quickstart.html#introduction   
+        http://answers.unity3d.com/questions/333829/including-a-dll-in-unity.html
+        https://forum.unity3d.com/threads/error-cannot-be-an-iterator-block-because-void-is-not-an-iterator-interface-type.144248/
+
+        ***http://answers.unity3d.com/questions/11021/how-can-i-send-and-receive-data-to-and-from-a-url.html
+        */
+
+        Debug.Log("Init furniture");
+
+
+
+        foreach (FurniturePairing pair in myRegistry.myPairings) {
+
+			Debug.Log ("MYPAIR is" + pair.FurnName);
+			pair.FurnObj = (GameObject)Resources.Load (pair.FurnName);
+			//Debug.Log ("Sprite type is " +Resources.Load (pair.IconName).GetType());
+
+			Texture2D tempText = (UnityEngine.Texture2D)Resources.Load (pair.IconName);
+			pair.myIcon = Sprite.Create( tempText,new Rect(0,0,tempText.width,tempText.height),Vector2.zero);
+		}
+	}
+	/*
+	void SerializeFurniture(){
+        Debug.Log("serialize");
+
+
+        string registryName = "FurnitureRegistry.txt";
+		string json = JsonUtility.ToJson (myRegistry);
+        File.Create(registryName);
+        File.SetAttributes(registryName, FileAttributes.Normal);
+        File.WriteAllText (registryName, json);
+	}
+    */
+	public FurniturePairing GetFurniture(int num){
+
+		foreach (FurniturePairing mat in myRegistry.myPairings){
+			if (mat.id == num) {
+				return mat;
+			}}
+		throw new System.Exception("GameObject Not Found!");
+
+	}
+
+	[Serializable]
+	public class FurniturePairing{
+
+		public string FurnName;
+		public int id;
+		public int DefaultMaterial;
+		public string IconName;
+		[NonSerialized]
+		public GameObject FurnObj;
+		[NonSerialized]
+		public Sprite myIcon;
+
+		public FurnitureType myType;
+
+	}
+    
+	[Serializable]
+	public class FurnitureRegistry
+	{
+		public List<FurniturePairing> myPairings;
+	}
+
+
+  
+    /*
+    private FurnitureRegistry ProcessRegistryJson(string jsonString)
+    {
+        // convert to json-like object with LitJSON library
+        JsonData jsonvalue = JsonMapper.ToObject(jsonString);
+        //Debug.Log(jsonvalue);
+        FurnitureRegistry newRegistry = new FurnitureRegistry();
+
+        // add each JSON value-field to a new FurniturePairing object, add the object to the Registry pairing list.
+        for (int i = 0; i < jsonvalue["myPairings"].Count; i++)
+        {
+            FurniturePairing newPair = new FurniturePairing();
+            newPair.FurnName = jsonvalue["myPairings"][i]["FurnName"].ToString();
+            newPair.id = (int) jsonvalue["myPairings"][i]["id"];
+            newPair.DefaultMaterial = (int) jsonvalue["myPairings"][i]["DefaultMaterial"];
+            newPair.IconName = jsonvalue["myPairings"][i]["IconName"].ToString();
+
+            newRegistry.myPairings.Add(newPair);
+        }
+
+        //return instatiated and mostly fleshed out FurniturePairing object.
+        return newRegistry;
+    }
+
+    */
+
+}
